565996486854d8c7e31ed0294a83e2eec4084779
juanpa <juan.paq72@mail.com>
2025-05-28 09:04:57 -0600

    url de expediente

--- BA/api/urls.py
+++ BA/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserListCreateView, UserDetailView
+from .views import UserListCreateView, UserDetailView, ExpedientesListCreateView, ExpedientesDetailView
 
 urlpatterns = [
     path('users/', UserListCreateView.as_view(), name='user-list-create'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('expedientes/', ExpedientesListCreateView.as_view(), name='expedientes-list-create'),
+    path('expedientes/<int:pk>/', ExpedientesDetailView.as_view(), name='expedientes-detail'),
 ]
