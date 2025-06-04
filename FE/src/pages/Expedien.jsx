50e534fd8d5a54c05cf0ecaa236ca42b98ac609d
jhared88nsx <jaguilar@fwdcostarica.com>
2025-06-04 13:26:00 -0600

    t

--- FE/src/pages/Expedien.jsx
+++ FE/src/pages/Expedien.jsx
@@ -1,12 +1,11 @@
 import Expediente from "../component/Expediente"
-
+import Navbar from "../component/Navbar"
 
 function Expedien() {
   return (
     <div>
-
+        <Navbar/>
         <Expediente />
-
     </div>
   )
 }
