8a36659626752c7ec713ba342c1d4f696fb1f581
juanpa <juan.paq72@mail.com>
2025-06-25 15:32:22 -0600

    documento view vista generando pdf

--- FE/src/component/Visita.jsx
+++ FE/src/component/Visita.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react'
 import Llamados from '../services/Llamados'
 import "../style/ExpeInput.css"
 import "../style/visita.css"
-import uploadImageToS3 from './Credenciales'
+import uploadImageToS3 from './credenciales'
 
 
 
