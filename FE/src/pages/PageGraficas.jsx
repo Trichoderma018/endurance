ece16f891d8cce8bfab333a799cd57a3531c822e
Juan Quesada <juan.paq72@gmail.com>
2025-08-21 18:45:01 -0600

    cambios realizados y correción de error

---
+++ FE/src/pages/PageGraficas.jsx
@@ -0,0 +1,12 @@
+import Report from "../component/Report"
+
+function PageGraficas() {
+  return (
+    <div style={{ marginTop: 1200, width: "500px" }}>
+        <Report/>
+
+    </div>
+  )
+}
+
+export default PageGraficas
