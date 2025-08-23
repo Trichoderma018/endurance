769b45335a8268e6bce84c95111dda4e47efb7ec
Juan Quesada <146791804+Trichoderma018@users.noreply.github.com>
2025-08-22 20:18:10 -0600

    Cambios en settings y Report

--- BA/BA/settings.py
+++ BA/BA/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'api',
     'ba',
     'corsheaders',
@@ -144,6 +145,15 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
 
 REST_FRAMEWORK = {
